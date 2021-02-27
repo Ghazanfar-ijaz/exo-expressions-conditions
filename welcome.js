8c592d50daf3a45d216fbc1e5f09aaaf03680b6b
Ghazanfar-ijaz <78793183+Ghazanfar-ijaz@users.noreply.github.com>
2021-02-27 13:55:35 +0100

    Add files via upload

---
+++ welcome.js
@@ -0,0 +1,4 @@
+let firstname = "Alice"
+let gender = "female"
+let msg = gender === 'male' ? `Bonjour ${firstname}, vous êtes entré sur le chan` : `Bonjour ${firstname}, vous êtes entrée sur le chan`
+console.log(msg)
